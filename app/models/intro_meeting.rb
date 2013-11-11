1ac389bb4e2f27741cc802ab6c3e3cbdc2f4d4a3
Scott Shillcock <scott@shillcock.net>
2013-11-10 18:36:25 -0800

    Added structure block to IntroMeeting class

--- app/models/intro_meeting.rb
+++ app/models/intro_meeting.rb
@@ -1,18 +1,11 @@
-# == Schema Information
-#
-# Table name: intro_meetings
-#
-#  id          :integer          not null, primary key
-#  title       :string(255)
-#  description :text
-#  starts_at   :datetime
-#  ends_at     :datetime
-#  created_at  :datetime
-#  updated_at  :datetime
-#
-
 class IntroMeeting < ActiveRecord::Base
-  validates :title, :starts_at, :ends_at, presence: true
+  structure do
+    title       :string,   "Free Intro Meeting", validates: :presence
+    starts_at   :datetime, Time.now,             validates: :presence
+    ends_at     :datetime, Time.now + 4.hours,   validates: :presence
+    description :text,     "Free meeting open to everyone."
+    timestamps
+  end
 
   def self.upcomming(limit=3)
     where('starts_at >= ?', Time.now).order(:starts_at).limit(limit)
