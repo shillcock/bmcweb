14ac20f5ba26c68a30f103a5e9f1a9e8f3d32b9c
Scott Shillcock <scott@shillcock.net>
2013-11-21 19:37:06 -0800

    added clearance views

--- db/migrate/20131111024142_create_users.rb
+++ db/migrate/20131111024142_create_users.rb
@@ -1,12 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-      t.string :email 
-      t.datetime :updated_at 
-      t.datetime :created_at 
+      t.string :email
+      t.datetime :updated_at
+      t.datetime :created_at
     end
   end
-  
+
   def self.down
     drop_table :users
   end
