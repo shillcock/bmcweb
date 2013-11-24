70f2b48c8c90834fa0a574ed308a3e87b29bf32d
Scott Shillcock <scott@shillcock.net>
2013-11-23 17:21:37 -0800

    annoated models

--- app/models/intro_meeting.rb
+++ app/models/intro_meeting.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: intro_meetings
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  description :text
+#  date        :date
+#  starts_at   :time             default(2000-01-01 03:00:00 UTC)
+#  ends_at     :time             default(2000-01-01 05:00:00 UTC)
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class IntroMeeting < ActiveRecord::Base
   validates :title, presence: true
   validates :date, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :integer          not null, primary key
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(128)
+#  confirmation_token :string(128)
+#  remember_token     :string(128)
+#
+
 require 'spec_helper'
 
 describe User do
