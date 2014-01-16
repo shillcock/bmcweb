d482256dbc1dc8cf5e3d13cf358dce20e07542e3
Scott Shillcock <scott@shillcock.net>
2014-01-16 13:17:55 -0800

    Add missing js and css to precompiled assets for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ BreakthroughForMen::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( admin.css admin.js welcome.css vendor/modernizr.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
