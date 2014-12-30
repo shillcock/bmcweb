9778023cac9e03fcab63a3ad056208eb439f68ab
Scott Shillcock <scott@shillcock.net>
2014-12-29 20:37:53 -0800

    Update default seed pw

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ def fake_user_params
   {
     name: "#{first_name} #{Faker::Name.last_name}",
     email: Faker::Internet.email(first_name),
-    password: 'test', #Faker::Internet.password(12),
+    password: 'test1234', #Faker::Internet.password(12),
     phone_number: Faker::PhoneNumber.cell_phone,
     address1: Faker::Address.street_address,
     city: Faker::Address.city,
