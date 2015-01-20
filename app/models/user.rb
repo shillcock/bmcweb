b4b057fedcb2ee9cd29cc11dafa2fc41bcce6281
Scott Shillcock <scott@shillcock.net>
2015-01-19 20:48:28 -0800

    Update to Rails 4.2 release

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,8 +29,12 @@
 #
 
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable, :recoverable,
-         :rememberable, :trackable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable
 
   has_paper_trail
 
